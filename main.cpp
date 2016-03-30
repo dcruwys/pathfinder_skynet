93aedd3cf08371e197d6e825f13386ad620e216e
Nathan Egan <egan.nathan@gmail.com>
2016-03-29 21:40:51 -0600

    adding partially finished bfs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "NaryTree.h"
+#include "Gameboard.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    NaryTree tree = NaryTree();
+    NaryTree::Action
+    NaryTree::State initial = new NaryTree::State(0);
+    NaryTree::State goal = new NaryTree::State(8);
+    BFS<NaryTree::State, NaryTree::Action, NaryTree> search = new BFS<NaryTree::State, NaryTree::Action, NaryTree>();
+    uint8_t nodesExpanded;
+    if(search.GetPath(tree, intial, goal))
+    {
+        nodesExpanded = search.GetNodesExpanded();
+    }
+    std::cout << nodesExpanded << std::endl;
+
+
+    std::getchar();
     return 0;
 }
 
