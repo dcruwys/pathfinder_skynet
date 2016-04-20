0eabe8cbdab2bb2eba297300b163d42bdf8d6630
nathansegan <egan.nathan@gmail.com>
2016-04-19 20:54:23 -0600

    ranking done, unranking not working
    hw3 initial commit.

--- SlidingTile.cpp
+++ SlidingTile.cpp
@@ -1,42 +1,203 @@
 #include "SlidingTile.h"
+#include <vector>
+#include <algorithm>
 
-void SlidingTile::GetActions(SlidingTile4x4::State &board, std::vector<SlidingTile4x4::Action> &actions)
+uint64_t Factorial(int temp)
 {
-	//clear list of actions before adding to it
-	actions.clear();
-	//find if can move left
-	if(board.blank % board.size != 0){
-        actions.push_back(SlidingTile4x4::direction::LEFT);
+	uint64_t table[17] =
+	{ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000};
+	return table[temp];
+}
+
+SlidingTile::SlidingTile()
+{
+	for (int x = 0; x < 16; x++)
+	{
+		tiles[x] = x;
 	}
-    if(board.blank % board.size != 3){
-        actions.push_back(SlidingTile4x4::direction::RIGHT);
-    }
-    if(board.blank / board.size != board.size-1){
-        actions.push_back(SlidingTile4x4::direction::DOWN);
-    }
-    if(board.blank / board.size != 0){
-        actions.push_back(SlidingTile4x4::direction::UP);
-    }
-
-
+	blank = 0;
 }
 
-void SlidingTile::ApplyAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a)
+SlidingTile::SlidingTile(const int temp[16])
 {
-    switch(a.myDirection){
-        case SlidingTile4x4::direction::LEFT: std::swap(s.board[s.blank], s.board[s.blank-1]); s.blank--;  break;
-        case SlidingTile4x4::direction::RIGHT: std::swap(s.board[s.blank], s.board[s.blank+1]); s.blank++; break;
-        case SlidingTile4x4::direction::UP: std::swap(s.board[s.blank], s.board[s.blank-s.size]); s.blank -= 4; break;
-        case SlidingTile4x4::direction::DOWN: std::swap(s.board[s.blank], s.board[s.blank+s.size]); s.blank += 4; break;
-    }
+	for (int x = 0; x < 16; x++)
+	{
+		if (temp[x] == 0)
+		{
+			blank = x;
+		}
+		tiles[x] = temp[x];
+	}
 }
 
-void SlidingTile::UndoAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a)
+uint64_t SlidingTile::Rank(const std::vector<int> pattern)
 {
-    switch(a.myDirection){
-        case SlidingTile4x4::direction::RIGHT: std::swap(s.board[s.blank], s.board[s.blank-1]); s.blank--; break;
-        case SlidingTile4x4::direction::LEFT: std::swap(s.board[s.blank], s.board[s.blank+1]); s.blank++; break;
-        case SlidingTile4x4::direction::DOWN: std::swap(s.board[s.blank], s.board[s.blank-s.size]); s.blank -= 4; break;
-        case SlidingTile4x4::direction::UP: std::swap(s.board[s.blank], s.board[s.blank+s.size]); s.blank += 4; break;
-    }
+	int locs[16] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+	std::vector<int> dual;
+	
+
+	// create dual array with locations of tiles
+	// that are not blanked out
+	for (int i = 0; i < pattern.size(); i++)
+	{
+		for (int j = 0; j < 16; j++)
+		{
+			// for each tile in our pattern
+			// set the ith value in our dual array
+			// to the location of the pattern value
+			// in our sliding tile
+			if (tiles[j] == pattern[i])
+			{
+				dual.push_back(j);
+				locs[pattern[i]] = j;
+			}
+		}	
+	}
+	uint64_t rankVal = 0;
+	int numEntriesLeft = 16;
+	// compute the lexographical ranking of the locations
+	// of the first 16 tiles
+	for (int x = 0; x < 16; x++)
+	{
+		if (locs[x] != -1)
+		{
+			rankVal += locs[x] * (Factorial(numEntriesLeft - 1) / Factorial(12));
+			numEntriesLeft--;
+		}
+		// decrement locations of remaining items
+		// to keep the numbering compact
+		if (locs[x] != -1)
+		{
+			for (int y = x; y < 16; y++)
+			{
+				if (locs[y] > locs[x])
+					locs[y]--;
+			}
+		}
+	}
+	return rankVal;
 }
+
+uint64_t SlidingTile::GetMaxRank(std::vector<int> &pattern)
+{
+	return Factorial(16) / Factorial(15 - pattern.size());
+}
+
+void SlidingTile::Unrank(uint64_t rank)
+{
+	int count = 16;
+	uint64_t hashVal = rank;
+	int dual[16];
+
+	// unrank the locations of the first 16 tiles
+	int numEntriesLeft = 1;
+	for (int x = 16 - 1; x >= 0; x--)
+	{
+		dual[x] = hashVal % numEntriesLeft;
+		hashVal /= numEntriesLeft;
+		numEntriesLeft++;
+		for (int y = x + 1; y < 16; y++)
+		{
+			if (dual[y] >= dual[x])
+				dual[y]++;
+		}
+	}
+	for (int i = 0; i < 16; i++)
+	{
+		std::cout << dual[i] << std::endl;
+	}
+	// clear puzzle locations
+	for (int x = 0; x < 16; x++)
+	{
+		tiles[x] = -1;
+	}
+	// revert locations of tiles into positions in the puzzle
+	for (int x = 0; x < 16; x++)
+	{
+		tiles[x] = dual[x];
+
+	}
+	// reset the cache of the blanks location
+	blank = dual[0];
+}
+
+void SlidingTile::Print()
+{
+	for (int i = 0, j = 0; i < 16; ++i, j++)
+	{
+		std::cout << tiles[i] << "\t";
+		if (j == 3){ std::cout << std::endl; j = -1; }
+		
+	}
+}
+
+//void SlidingTile::GetActions(SlidingTile &board, std::vector<Action> &actions)
+//{
+//	//clear list of actions before adding to it
+//	actions.clear();
+//	
+//	//push possible actions onto list of actions
+//	if (board.blankTileX != 0)
+//	{
+//		actions.push_back(SlidingTile4x4::Action(SlidingTile4x4::direction::LEFT));
+//	}
+//	if (board.blankTileX != board.cols - 1)
+//	{
+//		actions.push_back(SlidingTile4x4::Action(SlidingTile4x4::direction::RIGHT));
+//	}
+//	if (board.blankTileY != 0)
+//	{
+//		actions.push_back(SlidingTile4x4::Action(SlidingTile4x4::direction::DOWN));
+//	}
+//	if (board.blankTileY != board.rows - 1)
+//	{
+//		actions.push_back(SlidingTile4x4::Action(SlidingTile4x4::direction::UP));
+//	}
+//}
+//
+//void SlidingTile::ApplyAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a)
+//{
+//	switch (a.myDirection)
+//	{
+//	case SlidingTile4x4::direction::LEFT: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX - 1, s.blankTileY);
+//		s.blankTileX -= 1;
+//		break;
+//	case SlidingTile4x4::direction::RIGHT: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX + 1, s.blankTileY);
+//		s.blankTileX += 1;
+//		break;
+//	case SlidingTile4x4::direction::DOWN: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX, s.blankTileY - 1);
+//		s.blankTileY -= 1;
+//		break;
+//	case SlidingTile4x4::direction::UP: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX , s.blankTileY + 1);
+//		s.blankTileY += 1;
+//		break;
+//	default: std::cout << "Sliding Tile ApplyAction() not working correctly"; break;
+//	}
+//}
+//
+//void SlidingTile::UndoAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a)
+//{
+//	switch (a.myDirection)
+//	{
+//	case SlidingTile4x4::direction::RIGHT: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX - 1, s.blankTileY);
+//		s.blankTileX -= 1;
+//		break;
+//	case SlidingTile4x4::direction::LEFT: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX + 1, s.blankTileY);
+//		s.blankTileX += 1;
+//		break;
+//	case SlidingTile4x4::direction::UP: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX, s.blankTileY - 1);
+//		s.blankTileY -= 1;
+//		break;
+//	case SlidingTile4x4::direction::DOWN: 
+//		s.swapTiles(s.board, s.blankTileX, s.blankTileY, s.blankTileX, s.blankTileY + 1);
+//		s.blankTileY += 1;
+//		break;
+//	default: std::cout << "Sliding Tile ApplyAction() not working correctly"; break;
+//	}
--- SlidingTile.h
+++ SlidingTile.h
@@ -2,53 +2,32 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
-#include <math.h>
-namespace SlidingTile4x4 {
-    enum direction {
-        UP, DOWN, LEFT, RIGHT
-    }; //enum to represent possible actions of puzzle blank
 
+	enum direction{ UP, DOWN, LEFT, RIGHT }; //enum to represent possible actions of puzzle blank
 
-    struct State {
-        std::vector<int> board;
-        int blank;
-        int size;
-        direction lastAction;
+	struct Action{
+		direction myDirection; //give an action a direction
+		Action(direction directionIN){
+			//read a direction in in the constructor and set it to the direction of the Action
+			myDirection = directionIN;
+		}
+	};
 
-        State(std::vector<int> in) {
-            board = in;
-            size = sqrt(board.size());
-            auto i = find(in.begin(), in.end(), 0);
-            blank = distance(in.begin(), i);
-            std::cout << "Blank is at: " << blank << std::endl;
-        }
-    };
-
-    inline bool operator==(const State &lhs, const State &rhs) {
-        return lhs.board == rhs.board;
-    }
-
-    struct Action {
-        direction myDirection; //give an action a direction
-        Action() { }
-
-        Action(direction directionIN) {
-            //read a direction in in the constructor and set it to the direction of the Action
-            myDirection = directionIN;
-        }
-    };
-    inline bool operator!=(const Action &lhs, const Action &rhs) {
-        return lhs.myDirection != rhs.myDirection;
-    }
-
-}
 class SlidingTile {
 
 public:
-	SlidingTile(){}
-	void GetActions(SlidingTile4x4::State &nodeID, std::vector<SlidingTile4x4::Action> &actions);
-	void ApplyAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a);
-	void UndoAction(SlidingTile4x4::State &s, SlidingTile4x4::Action a);
+	SlidingTile();
+	SlidingTile(const int tiles[16]);
+	uint64_t Rank(std::vector<int> pattern);
+	uint64_t GetMaxRank(std::vector<int> &pattern);
+	void Unrank(uint64_t rank);
+	void GetActions(SlidingTile &nodeID, std::vector<Action> &actions);
+	void ApplyAction(SlidingTile &s, Action a);
+	void UndoAction(SlidingTile &s, Action a);
+	void Print();
+private:
+	int blank;
+	int tiles[16];
 };
 
 
