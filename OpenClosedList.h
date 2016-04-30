57c8be986b6d1d7938c21523665f62a2708c89f3
nathansegan <egan.nathan@gmail.com>
2016-04-30 12:40:40 -0600

    started setting up open/closed list api

--- OpenClosedList.h
+++ OpenClosedList.h
@@ -1,10 +1,52 @@
 #ifndef OpenClosedList_h
 #define OpenClosedList_h
+#include "OctileGrid.h"
+
+//TODO: do we need to create a struct to hold coordinate and cost?
+//TODO: Where to check duplicates from? Open? Closed? Both?
 
 class OpenClosedList
 {
 public:
+	void addOpen(coordinate &c);
+	void addClosed(coordinate &c);
+	bool checkDuplicates(coordinate &c);
+	void updateCost();
+	coordinate removeBest();
 private:
+	std::vector<coordinate> open;
+	std::vector<coordinate> closed;
 };
 
+void OpenClosedList::addOpen(coordinate &c)
+{
+	open.push_back(c);
+}
+
+void OpenClosedList::addClosed(coordinate &c)
+{
+	closed.push_back(c);
+}
+
+bool OpenClosedList::checkDuplicates(coordinate &c)
+{
+	for (int i = 0; i < open.size(); i++)
+	{
+		if (open[i] == c)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void OpenClosedList::updateCost()
+{
+
+}
+
+coordinate OpenClosedList::removeBest()
+{
+	
+}
 #endif
